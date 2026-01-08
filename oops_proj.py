59a73793f33116a5320e46cb942be1a4e376d0bf
sireesha <sireesha80.dhoorjati@gmail.com>
2026-01-08 09:40:14 +0530

    project basic structure ready

---
+++ oops_proj.py
@@ -0,0 +1,24 @@
+class chatbook:
+    def __init__(self):
+        self.username=''
+        self.password=''
+        self.loggedin=False
+        self.menu()
+    def menu(self):
+        user_input=input("""welcome to chatbook!! how would u like to proceed
+                         1. press 1 to signup
+                         2. press 2 to signin
+                         3. press 3 to write a post
+                         4. press aby other key to exit""")
+        if user_input=="1":
+            pass
+        if user_input=="2":
+            pass
+        if user_iput=="3":
+            pass
+        if user_input=="4":
+            pass
+        else:
+            exit()
+user1=chatbook()
+        
